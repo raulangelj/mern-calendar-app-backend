05494aa6c9c244d01bf18cbe03e28d738c9115a5
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-30 15:22:37 -0600

    ADD port to listen

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hola mundo')
+const express = require("express");
+
+// create express server
+const app = express();
+
+// create express router
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// listen to port
+app.listen(4000, () => {
+  console.log(`Server is running on port ${4000}`);
+});
